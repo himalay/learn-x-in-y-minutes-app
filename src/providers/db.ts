1503cc2d847776b50e36b3d8c9e91caad20a41f2
Himalay <contact@himalay.com.np>
2017-02-25 00:57:45 +0545

    Use lokijs and localstorage to handle data storage

--- src/providers/db.ts
+++ src/providers/db.ts
@@ -1,12 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import {Injectable} from '@angular/core';
+import * as loki from 'lokijs';
+import {Language} from '../interfaces';
 
 @Injectable()
 export class DbProvider {
+  db : any;
+  languages : any;
 
-  constructor(public http: Http) {
-    console.log('Hello Db Provider');
+  constructor() {
+    this.db = new loki('xiny');
+    this.languages = this.db.addCollection('languages');
+    this.importAll();
   }
 
+  insert(language : Language) {
+    this.languages.insert(language);
+    this.saveAll();
+  }
+
+  updateWhere($loki, content){
+    this.languages.chain()
+    .find({ $loki })
+    .update(language => {
+      language = Object.assign(language, content);
+      this.saveAll();
+    });
+  }
+
+  remove(language) {
+    this.languages.remove(language.$loki);
+    this.saveAll();
+  }
+
+  toArray(val) {
+    return Array.from(val);
+  }
+
+  saveAll() {
+    try {
+      localStorage.setItem('xiny', JSON.stringify(this.db));
+    } catch(e) {
+      // console.error(e);
+   }
+  }
+
+  importAll() {
+    let xiny;
+    try {
+        xiny = localStorage.getItem('xiny');
+    } catch(e) {
+        console.error(e);
+    }
+    
+    if (xiny) {
+      this.db.loadJSON(xiny);
+        this.languages = this.db.getCollection('languages');
+    }
+  }
 }
